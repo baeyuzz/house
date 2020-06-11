03f157b80d2ba91aa60ea9d78234937f5099c1ec
seow0 <seow0124@naver.com>
2020-06-11 12:13:16 +0900

    QnA list, detail css 적용

--- vue_project/src/components/QnaDetail.vue
+++ vue_project/src/components/QnaDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <!-- <div>
     <table>
       <tbody>
         <tr>
@@ -49,6 +49,52 @@
         </tr>
       </tbody>
     </table>
+  </div> -->
+
+   <div>
+    <section class="content-box">
+     <h2>Q&A</h2>
+      <div class="article-box">
+        <table class="table table-borderless shadow-lg p-3 rounded">
+          <tr>
+            <td class="border border-dark">{{qna.qna_no}}</td>
+            <td class="border border-dark">제목 : {{qna.qna_title}}</td>
+            <td class="border border-dark">작성자 : {{qna.qna_userid}}</td>
+            <td class="border border-dark">작성일 : {{qna.qna_datetime}}</td>
+          </tr>
+          <!-- <tr class="border border-bottom-0 border-dark bg-white">
+            <td colspan="2"></td>
+            <td class="text-muted">조회수: {{notice.hitCount}}</td>
+          </tr> -->
+          <tr class="bg-white">
+            <td
+              id="content-cell"
+              class="border border-top-0 border-dark text-left"
+              colspan="4"
+            >{{qna.qna_content}}</td>
+          </tr>
+        </table>
+        <div class="d-flex justify-content-end">
+          <router-link class="btn btn-primary m-2" to="/qna" title="목록으로">
+            <i class="fas fa-clipboard-list"></i>
+          </router-link>
+          <template v-if="qna.qna_userid == id">
+            <button
+              class="btn btn-warning m-2"
+             @click="update(qna.qna_no)"
+              title="수정하기">
+              <i class="fas fa-tools"></i>
+            </button>
+            <button
+              class="btn btn-danger m-2"
+              @click="deleteQnA(qna.qna_no)"
+              title="삭제하기">
+              <i class="fas fa-trash-alt"></i>
+            </button>
+          </template>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -77,10 +123,10 @@ export default {
   },
   methods: {
   
-    movePage() {
-      this.$router.push('/qna');
-    }
-    , update(no) {
+    // movePage() {
+    //   this.$router.push('/qna');
+    // }, 
+    update(no) {
       this.$router.push('/update/'+no); 
     }
     , deleteQnA(no) {
@@ -97,15 +143,20 @@ export default {
 </script>
 
 <style scoped>
-.addContainer{
-  background: linear-gradient(to right, #647811, #527810);
-  display: inline-block;
-  width: 3rem;
-  border-radius: 5px 5px 5px 5px;
-}
+
 .modifyBtn, .addBtn {
   color: white;
   vertical-align: middle;
 }
-
+.content-box {
+  text-align: center;
+}
+.article-box {
+  width: 700px;
+  margin: auto;
+}
+#content-cell {
+  max-height: 700px;
+  overflow: scroll;
+}
 </style>
--- vue_project/src/components/QnaList.vue
+++ vue_project/src/components/QnaList.vue
@@ -1,35 +1,82 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="(qna) in qnalist" v-bind:key="qna.qna_no" @click="getQna(qna.qna_no)">
-        {{qna.qna_no}} -  {{qna.qna_userid}} - {{qna.qna_title}} - {{qna.qna_content}} - {{qna.qna_datetime}}
-        <!-- {{qna.reply_content}} - {{qna.reply_datetime}} - {{qna.reply_userid}} -->
-      </li>
-    </ul>
-    <router-link v-if="name.length > 0" to="/addQna">추가</router-link> &nbsp;
+	<div>
+		<section>
+			<h1 style="text-align : center">Q & A</h1>
+			<form action="#" method="post">
+				<div class="form-inline sms-form-center">
+					<label class="mr-sm-2" for="title">Q&A 검색</label>
+          <input v-model.trim="title" class="form-control mb-2 mr-sm-2" type="text"
+						name="title" placeholder="검색할 단어를 입력하시오" />
+					<button @click.prevent="searchQna"
+						class="btn btn-success mb-2 mr-sm-2" type="submit">검색</button>
+				</div>
+			</form>
 
-  </div>
+			<div class="table-box">
+				<table class="table table-striped table-hover table-bordered">
+					<thead class="bg-secondary">
+						<tr>
+							<th>번호</th>
+							<th colspan="4">제목</th>
+							<th>작성자</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="qna in qnalist" v-bind:key="qna.qna_no" @click="getQna(qna.qna_no)">
+							<td>{{qna.qna_no}}</td>
+							<td colspan="4">{{qna.qna_title}}</td>
+							<td>{{qna.qna_userid}}</td>
+						</tr>
+					</tbody>
+				</table>
+
+				<div v-if="name.length > 0" class="d-flex justify-content-end">
+					<router-link class="btn btn-info mb-2 mr-sm-2" to="/addQna">
+					글쓰기</router-link>
+				</div>
+			</div>
+		</section>
+	</div>
+	<!-- searchQna 백엔드에 만들어야함
+      hit count도 만들어야함 -->
 </template>
 
-
-<script>
+	<script>
 import Constant from '../Constant';
+import http from "@/http-common.js";
+
 // import { mapActions } from 'vuex'
     export default {
-        computed : {
-            qnalist(){
-                return this.$store.state.qnaItems;
-            }
-            , name() {
-              return this.$store.state.name;
-            }
+      data(){
+        return {
+          title : "",
+        }
+      },
+      computed : {
+        qnalist(){
+          return this.$store.state.qnaItems;
+          },
+        name() {
+          return this.$store.state.name;
+          }
         },
-        created(){
-          console.log("created");
-          this.$store.dispatch(Constant.GET_QNALIST);
+      created(){
+        console.log("created");
+        this.$store.dispatch(Constant.GET_QNALIST);
         },
-         methods: {
-            getQna(no) {
+      methods: {
+        searchQna() {
+          http.post("/qna/search",{
+            title : this.title
+          })
+          .then(response => {
+            this.qnaist = response.data;
+          })
+          .catch(err => {
+            alert("Error : ", err);
+          });
+        },
+        getQna(no) {
             console.log('getQna -> '+ no);
             this.$router.push('/qna/'+no);
             },
@@ -39,6 +86,30 @@ import Constant from '../Constant';
   },
 };
 </script>
-<style scoped>
 
+<style scoped>
+.content-box {
+	text-align: center;
+}
+
+.table-box {
+	width: 700px;
+	margin: auto;
+}
+
+.sms-form-center {
+	justify-content: center;
+}
+
+.table {
+	margin-top: 20px;
+	table-layout: fixed;
+}
+
+td {
+	text-overflow: ellipsis;
+	overflow: hidden;
+	white-space: nowrap;
+}
 </style>
+
