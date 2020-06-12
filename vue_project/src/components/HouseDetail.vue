9f0113e75f0a7533417b6202ccbb60f57af69ba0
seow0 <seow0124@naver.com>
2020-06-12 19:47:43 +0900

    범죄 그래프 좀 고침

--- vue_project/src/components/HouseDetail.vue
+++ vue_project/src/components/HouseDetail.vue
@@ -221,16 +221,17 @@ export default {
         });
       }
     },
+    // 범죄 발생 그래프
     crimeChart(data) {
       let ctx = $("#crime-chart");
-      let chartObject = new Chart(ctx, {
+      new Chart(ctx, {
         type: "bar",
         data: {
           labels: data.labels,
           datasets: [
             {
               data: data.data,
-              label: "해당 구 강력 범죄 발생횟수",
+              label: "해당 구 강력 범죄 발생횟수 (2018년)",
               backgroundColor: "#ccebff",
               borderColor: "#0099ff",
               pointBackgroundColor: "#003d66"
@@ -238,40 +239,32 @@ export default {
           ]
         },
         options: {
-			responsive: true,
-			tooltips: {
-				enabled: false
-			},
-			hover: {
-				animationDuration: 0
-			},
-			animation: {
-				// duration: 1,
-				onComplete: function () {
-					var chartInstance = this.chart,
-						ctx = chartInstance.ctx;
-					ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-					ctx.fillStyle = 'black';
-					ctx.textAlign = 'center';
-					ctx.textBaseline = 'bottom';
+          responsive: true,
+          tooltips: {
+            enabled: false
+          },
+          hover: {
+            animationDuration: 0
+          },
+          animation: {
+            onComplete: function () {
+              var chartInstance = this.chart,
+                ctx = chartInstance.ctx;
+              ctx.fillStyle = 'black';
+              ctx.textAlign = 'center';
+              ctx.textBaseline = 'bottom';
 
-					this.data.datasets.forEach(function (dataset, i) {
-						var meta = chartInstance.controller.getDatasetMeta(i);
-						meta.data.forEach(function (bar, index) {
-							var data = dataset.data[index];							
-							ctx.fillText(data, bar._model.x, bar._model.y - 5);
-						});
-					});
-				}
-			},
-		}
-      });
-      ctx.click(e => {
-        let activePoints = chartObject.getElementsAtEvent(e);
-        if (activePoints.length > 0) {
-          activePoints[0]._index;
+              this.data.datasets.forEach(function (dataset, i) {
+                var meta = chartInstance.controller.getDatasetMeta(i);
+                meta.data.forEach(function (bar, index) {
+                  var data = dataset.data[index];							
+                  ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                });
+              });
+            }
+          },
         }
-      });  
+      });
     },
     resetDetail(no) {
       this.no = no;
