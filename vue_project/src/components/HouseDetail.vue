0997aa03732349231a23a592b1f4f7b491cb9851
seow0 <seow0124@naver.com>
2020-06-16 13:29:28 +0900

    House Detail Vue 로드뷰 띄우기~

--- vue_project/src/components/HouseDetail.vue
+++ vue_project/src/components/HouseDetail.vue
@@ -3,7 +3,14 @@
     <br>
     <div class="row">
       <div class="map-news-box col-4 container right-box">
-        <div id="map"></div>
+        <div class = "maps">
+          <div id = "mapWrapper" style="width:400px;height:100%;float:left">
+            <div id="map" style="width:100%;height:100%"></div>
+          </div>
+          <div id = "roadWrap" style="width:400px;height:300px;float:left">
+            <div id="roadview" style="width:100%;height:100%"></div> <!-- 로드뷰를 표시할 div 입니다 -->
+          </div>
+        </div>
         <button class="btn btn-primary" @click="reposition">건물 위치로 이동</button>
         <!-- NEWS -->
         <div style="text-align : left">
@@ -31,6 +38,7 @@
           </div>
         </div>
       </div>
+        
       <!-- 건물 정보 -->
       <div class="container content-box col-3">
         <div class="info-box table-responsive">
@@ -247,11 +255,15 @@ export default {
         center: coords,
         // 처음 표시될 확대/축소 레벨
         level: 3
+
       };
 
       // 지도를 만들어 컨테이너에 붙입니다.
       var map = new kakao.maps.Map(container, options);
+      map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW); //지도 위에 로드뷰 도로 올리기
       console.log("Make and attach map Complete");
+
+
 
       // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
       var mapTypeControl = new kakao.maps.MapTypeControl();
@@ -274,7 +286,66 @@ export default {
 
       console.dir(map);
       this.map = map;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      let rvContainer = document.getElementById('roadview'); //로드뷰를 표시할 div
+      let rv = new kakao.maps.Roadview(rvContainer); //로드뷰 객체
+      let rvClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
+      let mapWrapper = document.getElementById("mapWrapper");
+
+      toggleRoadview(coords);
+
+      let markImage = new kakao.maps.MarkerImage(
+          'https://t1.daumcdn.net/localimg/localimages/07/2018/pc/roadview_minimap_wk_2018.png',
+          new kakao.maps.Size(26, 46),
+          {
+              // 스프라이트 이미지를 사용합니다.
+              // 스프라이트 이미지 전체의 크기를 지정하고
+              spriteSize: new kakao.maps.Size(1666, 168),
+              // 사용하고 싶은 영역의 좌상단 좌표를 입력합니다.
+              // background-position으로 지정하는 값이며 부호는 반대입니다.
+              spriteOrigin: new kakao.maps.Point(705, 114),
+              offset: new kakao.maps.Point(13, 46)
+          }
+      );
+
+      let rvMarker = new kakao.maps.Marker({
+          image : markImage,
+          position: coords,
+          draggable: true,
+          map: map
+      });
+
+      kakao.maps.event.addListener(map, 'click', function(mouseEvent){
+          
+          // 현재 클릭한 부분의 좌표를 리턴 
+          var position = mouseEvent.latLng; 
+
+          rvMarker.setPosition(position);
+          toggleRoadview(position); //로드뷰를 토글합니다
+      });
+
+      function toggleRoadview(position){
+
+        //전달받은 좌표(position)에 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄웁니다
+        rvClient.getNearestPanoId(position, 50, function(panoId) {
+            if (panoId === null) {
+                rvContainer.style.display = 'none'; //로드뷰를 넣은 컨테이너를 숨깁니다
+                mapWrapper.style.heigth = '100%';
+                map.relayout();
+            } else {
+                // mapWrapper.style.height = '50%';
+                map.relayout(); //지도를 감싸고 있는 영역이 변경됨에 따라, 지도를 재배열합니다
+                rvContainer.style.display = 'block'; //로드뷰를 넣은 컨테이너를 보이게합니다
+                rv.setPanoId(panoId, position); //panoId를 통한 로드뷰 실행
+                rv.relayout(); //로드뷰를 감싸고 있는 영역이 변경됨에 따라, 로드뷰를 재배열합니다
+            }
+       });
+     } 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     },
+    
+
     reposition() {
       console.dir(this.map);
       this.map.panTo(this.currentPos);
@@ -497,11 +568,11 @@ li {
   overflow: auto;
   width: 90%;
 }
-#map {
-  width: 500px;
-  height: 400px;
-}
 .right-box{
   padding-left: 5%;
+}
+.maps {
+  width : 500;
+  height : 300px;
 }
 </style>
